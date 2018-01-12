80588f5edd5d0b56a2f41c264ac7ef5d7d12302f
Dennis Durling <djdtahoe@gmail.com>
2018-01-12 10:36:04 -0800

    that was quick

--- 005_data/01/main.go
+++ 005_data/01/main.go
@@ -6,13 +6,19 @@ import (
 	"text/template"
 )
 
+// creates a pointer (container for) to a Template "Type"
 var tpl *template.Template
 
 func init() {
+	// init function gets run first
+	// populate the tpl pointer with a Template object
+	// pointing to a specific template (tpl.gohtml in this case)
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
 
 func main() {
+	// ExecuteTemplate takes a writer (Stdout here) a template name
+	// and possibly values or nil and executes it
 	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 42)
 	if err != nil {
 		log.Fatalln(err)
