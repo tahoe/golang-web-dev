73d5d8c41d370c72925209ebd7e00194b8e6c17d
Dennis Durling <djdtahoe@gmail.com>
2018-01-12 10:27:20 -0800

    adding documentation

--- 004_parse_execute/05_performant-parsing_func-init/main.go
+++ 004_parse_execute/05_performant-parsing_func-init/main.go
@@ -6,9 +6,20 @@ import (
 	"text/template"
 )
 
+// creates a pointer to the template object, basically creating an empty object that
+// has all the methods but no data yet.
 var tpl *template.Template
 
+// func init() seems to be a standard function that gets auto run just like main
+// but probably before main
 func init() {
+	// 
+	// populate the pointer object using ParseGlob
+	// function call (Must) taking as an argument the results of a function call (ParseGlob)
+	// template.Must takes in a template pointer and an error as it's input
+	// which is exactly what most of the parsing methods return
+	// this would be a good place (the Must method) to do auto error codes and whatnot
+	// 
 	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
